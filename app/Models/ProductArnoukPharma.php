352c611992fb8b4713b511ac7393a6ae93ff8d84
Mohamad Ghazi Mohamad <107316607+MohamadGhMohamad37@users.noreply.github.com>
2025-04-16 10:03:59 -0400

    update file migration for product

--- app/Models/ProductArnoukPharma.php
+++ app/Models/ProductArnoukPharma.php
@@ -8,12 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class ProductArnoukPharma extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'name_en', 'name_ar', 'description_en', 'description_ar', 'main_image', 'images'
-    ];
-    
-    protected $casts = [
-        'images' => 'array', // حتى يتم التعامل مع الصور كمصفوفة
-    ];
-    
 }
---
+++ database/migrations/2025_04_16_140212_create_product_arnouk_pharmas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('product_arnouk_pharmas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_en');
+            $table->string('name_ar');
+            $table->text('description_en')->nullable();
+            $table->text('description_ar')->nullable();
+            $table->string('main_image')->nullable(); // صورة واحدة
+            $table->json('images')->nullable();       // مجموعة صور
+            $table->foreignId('category_arnouk_pharma_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_arnouk_pharmas');
+    }
+};
