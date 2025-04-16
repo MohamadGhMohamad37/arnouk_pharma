d8d958e430a064f05d68df0bf1aa54f145427e24
yokigh <ghassan.sheikh11@gmail.com>
2025-04-16 17:00:27 +0300

    about page

--- resources/views/website/arnouk_pharma/about/index.blade.php
+++ resources/views/website/arnouk_pharma/about/index.blade.php
@@ -68,37 +68,27 @@
      gallery
     =========================== -->
     <section class="gallery pt-10 pb-90">
-        <div class="container">
+      <div class="container">
           <div class="row">
-            <div class="col-12">
-              <div class="slick-carousel"
-                data-slick='{"slidesToShow": 4, "slidesToScroll": 1, "autoplay": true, "arrows": true, "dots": false, "responsive": [ {"breakpoint": 992, "settings": {"slidesToShow": 2}}, {"breakpoint": 767, "settings": {"slidesToShow": 2}}, {"breakpoint": 480, "settings": {"slidesToShow": 1}}]}'>
-                <a class="popup-gallery-item" href="{{asset('assets/images/gallery/1.jpg')}}">
-                    <img src="{{asset('assets/images/gallery/1.jpg')}}" alt="gallery img">
-                  </a>
-                  <a class="popup-gallery-item" href="{{asset('assets/images/gallery/1.jpg')}}">
-                    <img src="{{asset('assets/images/gallery/1.jpg')}}" alt="gallery img">
-                  </a>
-                  <a class="popup-gallery-item" href="{{asset('assets/images/gallery/1.jpg')}}">
-                    <img src="{{asset('assets/images/gallery/1.jpg')}}" alt="gallery img">
-                  </a>
-                  <a class="popup-gallery-item" href="{{asset('assets/images/gallery/1.jpg')}}">
-                    <img src="{{asset('assets/images/gallery/1.jpg')}}" alt="gallery img">
-                  </a>
-                  <a class="popup-gallery-item" href="{{asset('assets/images/gallery/5.png')}}">
-                    <img src="{{asset('assets/images/gallery/5.png')}}" alt="gallery img">
-                  </a>
-                  <a class="popup-gallery-item" href="{{asset('assets/images/gallery/1.jpg')}}">
-                    <img src="{{asset('assets/images/gallery/1.jpg')}}" alt="gallery img">
-                  </a>
-                  <a class="popup-gallery-item" href="{{asset('assets/images/gallery/1.jpg')}}">
-                    <img src="{{asset('assets/images/gallery/1.jpg')}}" alt="gallery img">
-                  </a>
-              </div><!-- /.gallery-images-wrapper -->
-            </div><!-- /.col-xl-5 -->
-          </div><!-- /.row -->
-        </div><!-- /.container -->
-      </section><!-- /.gallery 2 -->
+              <div class="col-12">
+                  <div class="slick-carousel"
+                      data-slick='{"slidesToShow": 4, "slidesToScroll": 1, "autoplay": true, "arrows": true, "dots": false, "responsive": [{"breakpoint": 992, "settings": {"slidesToShow": 2}}, {"breakpoint": 767, "settings": {"slidesToShow": 2}}, {"breakpoint": 480, "settings": {"slidesToShow": 1}}]}'>
+                      
+                      @php
+                          $images = $about && $about->images ? json_decode($about->images, true) : [];
+                      @endphp
+  
+                      @foreach ($images as $image)
+                          <a class="popup-gallery-item" href="{{ asset( $image) }}">
+                              <img src="{{ asset( $image) }}" alt="gallery img">
+                          </a>
+                      @endforeach
+  
+                  </div><!-- /.gallery-images-wrapper -->
+              </div>
+          </div>
+      </div>
+  </section>
 
     <!-- ========================
       About Layout 1
@@ -108,21 +98,19 @@
         <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-6">
             <div class="heading-layout2">
-              <h3 class="heading__title mb-40">Improving The Quality Of Your Life Through Better Health.</h3>
+              <h3 class="heading__title mb-40">{{ $lang === 'ar' ? $about->name_ar : $about->name_en }}</h3>
             </div><!-- /heading -->
           </div><!-- /.col-12 -->
         </div><!-- /.row -->
         <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-6">
             <div class="about__Text">
-              <p class="mb-30">Our goal is to deliver quality of care in a courteous, respectful, and compassionate
-                manner. We hope you will allow us to care for you and to be the first and best choice for healthcare.
-              </p>
+              <p class="mb-30">{!! $lang === 'ar' ? $about->description_ar : $about->description_en !!}</p>
             </div>
           </div><!-- /.col-lg-6 -->
           <div class="col-sm-12 col-md-12 col-lg-6">
             <div class="video-banner">
-              <img src="{{asset('assets/images/about/1.jpg')}}" alt="about">
+              <img src="{{asset($about->main_image)}}" alt="about">
             </div><!-- /.video-banner -->
           </div><!-- /.col-lg-6 -->
         </div><!-- /.row -->
@@ -130,8 +118,8 @@
         <div class="row pb-20 pt-20">
             <div class="col-sm-12 col-md-12 col-lg-12">
                 <div class="video-banner">
-                    <img src="{{asset('assets/images/about/1.jpg')}}" alt="about">
-                    <a class="video__btn video__btn-white popup-video" href="{{asset('assets/videos/my-video.mp4')}}">
+                    <img src="{{asset($about->main_image)}}" alt="about">
+                    <a class="video__btn video__btn-white popup-video" href="{{asset($about->video)}}">
                         <div class="video__player">
                             <i class="fa fa-play"></i>
                         </div>
@@ -144,10 +132,6 @@
 
     <button id="scrollTopBtn"><i class="fas fa-long-arrow-alt-up"></i></button>
   </div><!-- /.wrapper -->
-
-  <script src="assets/js/jquery-3.5.1.min.js"></script>
-  <script src="assets/js/plugins.js"></script>
-  <script src="assets/js/main.js"></script>
 </body>
 
 </html>
