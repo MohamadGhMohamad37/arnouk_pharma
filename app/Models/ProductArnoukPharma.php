dbeeaa5bd72d82ab9fe745af08830825391b9d23
Mohamad Ghazi Mohamad <107316607+MohamadGhMohamad37@users.noreply.github.com>
2025-04-16 10:28:55 -0400

    create model product arnouk pharma

--- app/Models/ProductArnoukPharma.php
+++ app/Models/ProductArnoukPharma.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductArnoukPharma extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name_en',
+        'name_ar',
+        'description_en',
+        'description_ar',
+        'main_image',
+        'images',
+        'category_arnouk_pharma_id',
+    ];
+
+    protected $casts = [
+        'images' => 'array', // لتحويل الصور من JSON إلى array تلقائياً
+    ];
+
+    // علاقة مع التصنيف (CategoryArnoukPharma)
+    public function category()
+    {
+        return $this->belongsTo(CategoryArnoukPharma::class, 'category_arnouk_pharma_id');
+    }
 }
