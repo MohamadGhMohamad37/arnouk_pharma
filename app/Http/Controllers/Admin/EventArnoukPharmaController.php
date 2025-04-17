6f43464de2bf7c662adc99e7f78daa1451879552
Mohamad Ghazi Mohamad <107316607+MohamadGhMohamad37@users.noreply.github.com>
2025-04-17 13:21:22 -0400

    build page show Event arnouk pharma

--- app/Http/Controllers/Admin/EventArnoukPharmaController.php
+++ app/Http/Controllers/Admin/EventArnoukPharmaController.php
@@ -129,7 +129,7 @@ public function update($lang,Request $request, EventArnoukPharma $event_arnouk_p
     return redirect()->route('event_arnouk_pharma.index', ['lang' => app()->getLocale()])->with('success', 'Event updated!');
 }
 public function show($lang,EventArnoukPharma $event_arnouk_pharma){
-    return view('admin.arnouk_pharma.about.show',compact('event_arnouk_pharma'));
+    return view('admin.arnouk_pharma.event.show',compact('event_arnouk_pharma'));
 }
 
 public function destroy($lang,EventArnoukPharma $event_arnouk_pharma)
